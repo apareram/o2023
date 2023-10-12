59c6baeb02375f5df85d7e5ba1e51db7f3f07a70
Alberto Parera <a2091860@ibero.uia.mx>
2023-10-12 10:50:08 -0600

    varias cosas de lista doble

---
+++ clases/listasDobles/funLisDub.c
@@ -0,0 +1,72 @@
+#include "tiposDoble.h"
+
+extern void insertarListaDoble(refs *nav, alum dat)
+{
+    nodo *nuevo;
+
+    nuevo = (nodo *)malloc(sizeof(nodo));
+    if(nuevo == NULL)
+    {
+        printf("\nNo hay memoria disponible.\n");
+        exit(1);
+    }
+
+    nuevo->izq = NULL;
+    nuevo->der = NULL;
+    nuevo->datos = dat;
+
+    if((nav->inicio == NULL) && (nav->fin == NULL))
+    {
+        nav->inicio = nuevo;
+        nav->fin = nuevo;
+    }
+    else
+    {
+        nuevo->izq = nav->fin;
+        nav->fin->der = nuevo;
+        nav->fin = nuevo;
+    }
+
+    return;
+}
+
+extern void imprimirListaDoble(refs nav)
+{
+    nav.aux = nav.inicio;
+
+    while(nav.aux != NULL)
+    {
+        printf("\nCueta: %d\t", nav.aux->datos.cuenta);
+        printf("Nombre: %s\t", nav.aux->datos.nombre);
+        printf("Promedio: %f\n", nav.aux->datos.promedio);
+        nav.aux = nav.aux->der;
+    }
+
+    return;
+}
+
+extern void borrarPrimero(refs *nav)
+{
+    nav->aux = nav->inicio;
+    if((nav->inicio == NULL) && (nav->fin == NULL))
+    {
+        printf("\nNo se puede borrar un nodo, la lista se encuentra vacía.\n");
+    }
+    else
+    {
+        if(nav->inicio == nav->fin)
+        {
+            nav->inicio = NULL;
+            nav->fin = NULL;
+            free(nav->aux);
+        }
+        else
+        {
+            nav->inicio = nav->inicio->der;
+            nav->inicio->izq = NULL;
+            free(nav->aux);
+        }
+    }
+
+    return;
+}
---
+++ clases/listasDobles/listaDoble.c
@@ -0,0 +1,58 @@
+/*
+@file listaDoble.c
+@brief Este es un ejemplo para construir una lista doblemente enlazada
+       simple.
+       A) Haga un programa que agregue datos de alumnos que están en un
+          archivo de texto alumnos.txt y los integre a la lista en orden
+          del apuntador der (derecha). Inserta conforme están capturados
+          en el archivo.
+          A.1) Imprima la lista 
+       B) Haga una función que borre/atienda el nodo del inicio de la lista
+          sin romper la integridad.
+          B.1) Borre la lista completa.
+          B.2) Haga un modulo/función que permita borrar un neodo que tenga
+               el número de cuenta que indique el usuario.
+       C) Cree una lista completa  que inserte los nodos en orden acendente
+          de la cuenta único del usuario.
+@author Alberto Parera Méndez en la clase de Guillermo Gómez
+@date 12/10/2023
+*/
+#include "tiposDoble.h"
+
+void insertarListaDoble(refs *nav, alum dat);
+void imprimirListaDoble(refs nav);
+void borrarPrimero(refs *nav);
+
+int main (int argc, char *argv[])
+{
+    FILE *fp;
+    alum datos;
+    refs navegador;
+
+    navegador.inicio = NULL;
+    navegador.fin = NULL;
+    navegador.aux = NULL;
+
+    fp = fopen(argv[1], "r");
+    if(fp == NULL)
+    {
+        printf("\nArchivo no disponible.\n");
+        exit(1);
+    }
+    while(fscanf(fp, "%d\t %[^\t]%f\n", &datos.cuenta, datos.nombre, &datos.promedio) == 3)
+    {
+        insertarListaDoble(&navegador, datos);
+    }
+    fclose(fp);
+
+    imprimirListaDoble(navegador);
+
+    printf("\nSe borrara un nodo: \n");
+    borrarPrimero(&navegador);
+    imprimirListaDoble(navegador);   
+    printf("\nSe borrara otro nodo: \n");
+    borrarPrimero(&navegador);
+    imprimirListaDoble(navegador);
+
+    return 0;
+}
