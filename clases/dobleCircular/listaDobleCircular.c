3516cd5fbd99c8949672124911e44287e20d5bbd
Alberto Parera <a2091860@ibero.uia.mx>
2023-10-17 10:49:36 -0600

    se crearon los prototipos de listasDoblesCirculare

---
+++ clases/dobleCircular/listaDobleCircular.c
@@ -0,0 +1,18 @@
+/*
+@file listaDobleCircular.c
+@brief Este es un programa permite crear una lista doblemente circular
+       con los datos de alumnos.txt.
+       1) Haga una función que cree una lista doble enlazada.
+       2) Verifique con otra finción la integridad de la lista creada.
+       3) Cree una segunda lista doble circular que ordene en orden 
+          acendente por el número de cuenta.
+@author Alberto Parera Méndez en la clase de Guillermo Gómez
+@date 17/10/2023
+*/
+#include "tiposDoble.h"
+
+int main (int argc, char *argv[])
+{
+
+    return 0;
+}
