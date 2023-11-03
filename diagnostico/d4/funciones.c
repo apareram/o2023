753b33c71b02599fc2f20a7623b228c3439a205d
Alberto Parera <a2091860@ibero.uia.mx>
2023-11-03 12:41:30 -0600

    se documento el programa de las funciones

--- diagnostico/d4/funciones.c
+++ diagnostico/d4/funciones.c
@@ -1,5 +1,12 @@
 #include "defTipos.h"
 
+/*
+ * @brief Esta función coloca los enteros que se encuentren en un archivo de texto en arreglo de enteros.
+ * @author Alberto Parera Méndez 
+ * @param nomArch es un apuntador al nombre del archivo recibido en la línea de comandos.
+ * @param arr[] es un arreglo de enteros.
+ * @return tam es un entero conteniendo el total de enteros en el archivo de texto.
+ */
 extern void colocarEnteros(char nomArch[], int arr[])
 {
     int i = 0, numero;
@@ -22,6 +29,13 @@ extern void colocarEnteros(char nomArch[], int arr[])
     return;
 }
 
+/*
+ * @brief Esta función calcula el tamaño del cual va a ser el arreglo, para esto cuenta los enteros en 
+          el archivo de texto.
+ * @author Alberto Parera Méndez 
+ * @param nomArch es un apuntador al nombre del archivo recibido en la línea de comandos.
+ * @return tam es un entero conteniendo el total de enteros en el archivo de texto.
+ */
 extern int calcularTam(char nomArch[])
 {
     FILE *fp;
@@ -42,6 +56,14 @@ extern int calcularTam(char nomArch[])
     return tam;
 }
 
+/*
+ * @brief Esta función recibe un arreglo con números deordenados y el tamaño de dicho arreglo, e imprime
+          el contenido del arreglo.
+ * @author Alberto Parera Méndez 
+ * @param arr[] es un arreglo de enteros.
+ * @param size es un entero que indíca el tamaño del arreglo.
+ * @return pt es un puntero de tipo nodo que apunta al raíz del árbol construido.
+ */
 extern void imprimirArreglo(int arr[], int size)
 {
     int i;
@@ -54,6 +76,14 @@ extern void imprimirArreglo(int arr[], int size)
     return;
 }
 
+/*
+ * @brief Esta función recibe un puntero de referencia y un entero; construye un árbol jerárquico 
+          ascendente (del nivel 0 al n).
+ * @author Alberto Parera Méndez 
+ * @param *pt es un puntero de tipo nodo.
+ * @param *num es un entero.
+ * @return pt es un puntero de tipo nodo que apunta al raíz del árbol construido.
+ */
 nodo *insertarNodArbol(nodo *pt, int num)
 {
     nodo *nuevo, *aux;
@@ -112,6 +142,11 @@ nodo *insertarNodArbol(nodo *pt, int num)
     return pt;
 }
 
+/*
+ * @brief Esta función recibe un entero y lo guarda en un archivo de texto
+ * @author/autor Alberto Parera Méndez
+ * @param entero es un número entero
+ */
 void guardarOrdEnt(int entero)
 {
     FILE *fp;
@@ -130,6 +165,14 @@ void guardarOrdEnt(int entero)
    return;
 }
 
+/*
+ * @brief Esta función recibe un puntero de referencia y recorre el árbol de izquierda a derecha de 
+          forma recursiva.
+ * Imprime en pantalla el recorrido.
+ * @author Alberto Parera Méndez
+ * @param *aux es un puntero de tipo nodo.
+ * @return aux es un puntero de tipo nodo que apunta al raíz del árbol construido.
+ */
 extern void recorrer(nodo *aux) // en orden ascendente
 {
     if(aux != NULL)
@@ -154,6 +197,13 @@ void imprimirEnTxt(nodo *aux, FILE *fp2)
     return;
 }
 
+/*
+ * @brief Esta función recibe un entero ingresado por el usuario y busca si se encuentra en el árbol
+ * @author Alberto Parera Méndez
+ * @param *aux es un puntero de tipo nodo.
+ * @param busca es un entero ingresado po el usuario
+ * @return aux es un puntero de tipo nodo que apunta al entero ingresado por el usuario.
+ */
 extern nodo *buscarDirecto(int busca, nodo *aux)
 {
     int comparaciones = 1;
@@ -176,6 +226,13 @@ extern nodo *buscarDirecto(int busca, nodo *aux)
     return aux;
 }
 
+/*
+ * @brief Esta función lee lo que se encuentra en un archivo de texto e ingresa los datos en un árbol
+          utilizando la función insertarNodArbol
+ * @author Alberto Parera Méndez
+ * @param nomArch es un puntero de un archivo de texto.
+ * @param raiz es un doble puntero que apunta a la direccion de la direccion del nodo raíz del árbol.
+ */
 extern void crearArbol(char nomArch[], nodo **raiz)
 {
     int numero, resultado;
