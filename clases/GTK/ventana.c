9f627e6c174820346d9f3e2fe712c7f581abebdd
Alberto Parera <a2091860@ibero.uia.mx>
2023-11-24 12:25:47 -0600

    se añadieron unos widgets de texto

--- clases/GTK/ventana.c
+++ clases/GTK/ventana.c
@@ -7,6 +7,7 @@ gboolean delete_event_handler(GtkWidget *widget, GdkEvent *event, gpointer user_
 int main(int argc, char *argv[])
 {
     GtkWidget *top_widget, *box, *label, *separator, *exit_btn;
+    GtkWidget *texto, *scrolWin;
     /* 1. Se inicializa el entorno */
     gtk_init(&argc, &argv);
 
@@ -27,14 +28,14 @@ int main(int argc, char *argv[])
     gtk_widget_set_name(exit_btn, "exit");
 
     /* 3. Registro de callbacks */
-    g_signal_connect(G_OBJECT(exit_btn), "clicked", G_CALLBACK(print_and_quit),
-     NULL);
+    g_signal_connect(G_OBJECT(exit_btn), "clicked", G_CALLBACK(print_and_quit), NULL);
     g_signal_connect(G_OBJECT(top_widget), "delete_event",
     G_CALLBACK(delete_event_handler), NULL);
     g_signal_connect(G_OBJECT(top_widget), "destroy",
     G_CALLBACK(print_and_quit), NULL);
 
     /* 4. Define jerarquía de instancias (pack the widgets)*/
+    gtk_container_add(GTK_CONTAINER(scrolWin), texto);
     gtk_container_add(GTK_CONTAINER(top_widget), box);
     gtk_box_pack_start_defaults(GTK_BOX(box), exit_btn);
     gtk_box_pack_start_defaults(GTK_BOX(box), separator);
