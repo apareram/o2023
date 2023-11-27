1223de87b4b4fa07fc02be6fe098c01780b22d16
Alberto Parera <a2091860@ibero.uia.mx>
2023-11-26 21:13:08 -0600

    se creó la ventana de aviso

--- practicas/P3/appEbook.c
+++ practicas/P3/appEbook.c
@@ -4,6 +4,7 @@ gboolean delete_event_handler(GtkWidget *widget, GdkEvent *event, gpointer user_
 void closeTheApp(GtkWidget *botSalir, gpointer data);
 void visualizarVentanaCrear(GtkWidget *botCrear, gpointer pVentana);
 void visualizarVentanaEditar(GtkWidget *botEditar, gpointer pVentana);
+void visualizarVentanaAviso(GtkWidget *botEditar, gpointer pVentana);
 
 /*
 void print_and_quit(GtkButton *was_clicked, gpointer user_data);
@@ -21,6 +22,8 @@ int main(int argc, char *argv[])
     GtkWidget *numSeccLbl, *titLbl;
     GtkWidget *hBox31, *hBox32, *hBox33, *hBox34, *vBox3;    
     GtkWidget *numSeccLbl2, *titLbl2, *pagLbl;
+    GtkWidget *hBox41, *vBox4;    
+    GtkWidget *avisoLbl, *texto1Lbl, *texto2Lbl;
 
     //1. inicializar entorno
     gtk_init(&argc, &argv);
@@ -54,7 +57,7 @@ int main(int argc, char *argv[])
     hBox34 = gtk_hbox_new(FALSE, 10);
     vBox3 = gtk_vbox_new(FALSE, 10);
     titLbl2 = gtk_label_new("Título de libro:");
-    numSeccLbl2 = gtk_label_new("Número de secciones:");
+    numSeccLbl2 = gtk_label_new("Número de sección:");
     pagLbl = gtk_label_new("Número de página:");
     miApp.edTitLbl = gtk_entry_new();
     miApp.edSeccLbl = gtk_entry_new();
@@ -62,6 +65,14 @@ int main(int argc, char *argv[])
     miApp.edBotReg = gtk_button_new_with_label("Regresar");
     miApp.edBotEdit = gtk_button_new_with_label("Editar");
 
+    window4 = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    hBox41 = gtk_hbox_new(FALSE, 10);
+    vBox4 = gtk_vbox_new(FALSE, 10);
+    avisoLbl = gtk_label_new("AVISO");
+    texto1Lbl = gtk_label_new("insetar instrucciones 1");
+    texto2Lbl = gtk_label_new("insertar instrucciones 2");
+    miApp.avBotReg = gtk_button_new_with_label("Regresar");
+    miApp.avBotSig = gtk_button_new_with_label("Siguiente");
 
     gtk_window_set_title(GTK_WINDOW(window1), "app Ebook");
     gtk_container_set_border_width(GTK_CONTAINER(window1), 100);
@@ -72,11 +83,15 @@ int main(int argc, char *argv[])
     gtk_window_set_title(GTK_WINDOW(window3), "Editar Ebook");
     gtk_container_set_border_width(GTK_CONTAINER(window3), 100);
 
+    gtk_window_set_title(GTK_WINDOW(window4), "AVISO");
+    gtk_container_set_border_width(GTK_CONTAINER(window4), 100);
+
     //3. Registro de callbacks
     g_signal_connect(G_OBJECT(window1), "delete_event", G_CALLBACK(delete_event_handler), NULL);
     g_signal_connect(G_OBJECT(botSalir), "clicked", GTK_SIGNAL_FUNC(closeTheApp), NULL);
     g_signal_connect(G_OBJECT(botCrear), "clicked", GTK_SIGNAL_FUNC(visualizarVentanaCrear), window2);
     g_signal_connect(G_OBJECT(botEditar), "clicked", GTK_SIGNAL_FUNC(visualizarVentanaEditar), window3);
+    g_signal_connect(G_OBJECT(miApp.botCrear), "clicked", GTK_SIGNAL_FUNC(visualizarVentanaAviso), window4);
 
     //4. Definiendo jerarquias
     gtk_box_pack_start_defaults(GTK_BOX(vBox1), bienvenidoLbl);
@@ -111,6 +126,14 @@ int main(int argc, char *argv[])
     gtk_box_pack_start_defaults(GTK_BOX(vBox3), hBox33);
     gtk_box_pack_start_defaults(GTK_BOX(vBox3), hBox34);
     gtk_container_add(GTK_CONTAINER(window3), vBox3);
+
+    gtk_box_pack_start_defaults(GTK_BOX(vBox4), avisoLbl);
+    gtk_box_pack_start_defaults(GTK_BOX(vBox4), texto1Lbl);
+    gtk_box_pack_start_defaults(GTK_BOX(vBox4), texto2Lbl);
+    gtk_box_pack_start_defaults(GTK_BOX(hBox41), miApp.avBotReg);
+    gtk_box_pack_start_defaults(GTK_BOX(hBox41), miApp.avBotSig);
+    gtk_box_pack_start_defaults(GTK_BOX(vBox4), hBox41);
+    gtk_container_add(GTK_CONTAINER(window4), vBox4);
 
     //5. Mostrar los widgets
     gtk_widget_show_all(window1);
--- practicas/P3/funGTKbook.c
+++ practicas/P3/funGTKbook.c
@@ -31,6 +31,13 @@ extern void visualizarVentanaEditar(GtkWidget *botAlta, gpointer pVentana)
     return;
 }
 
+extern void visualizarVentanaAviso(GtkWidget *botAlta, gpointer pVentana)
+{
+    gtk_widget_show_all(pVentana);
+
+    return;
+}
+
 extern void on_insert_text(GtkTextBuffer *buffer, GtkTextIter *location, gchar *text, gint len, gpointer data) 
 {
     GtkTextIter start, end;
